8f6c5f8595e53ac319d0eb1b56a3080a5e39507f
alaminyusuf <alaminyusuf131@gmail>
2021-03-30 12:10:22 +0100

    updated user Model

--- server/db/models/userModel.ts
+++ server/db/models/userModel.ts
@@ -1,33 +1,37 @@
-import mongoose, { Schema, Document } from 'mongoose'
+import mongoose, { Schema, Document } from 'mongoose';
 
 export interface IUser extends Document {
-    firstName: string;
-    lastName: string;
-    email: string;
-    createdAt?: string
+	firstName: string;
+	lastName: string;
+	email: string;
+	createdAt?: string;
 }
 
 const userSchema: Schema = new Schema({
-    firstName: {
-        type: String,
-        required: true
-    },
+	firstName: {
+		type: String,
+		required: true,
+	},
 
-    lastName: {
-        type: String,
-        required: true
-    },
+	lastName: {
+		type: String,
+		required: true,
+	},
 
-    email: {
-        type: String,
-        required: true
-    },
+	email: {
+		type: String,
+		required: true,
+	},
 
-    createdAt: {
-        type: Date,
-        default: Date.now,
-    },
+	password: {
+		type: String,
+		required: true,
+	},
 
-})
+	createdAt: {
+		type: Date,
+		default: Date.now,
+	},
+});
 
 export default mongoose.model<IUser>('User', userSchema);
