aba2870e7f84e0f54691ddaf97769f705edd23e5
alaminyusuf <alaminyusuf131@gmail>
2021-04-01 13:55:38 +0100

    added validation util

---
+++ server/src/graphql/utils/validationUtil.ts
@@ -0,0 +1,41 @@
+import { InputOptions } from './inputOptions';
+
+export const validationUtil = (options: InputOptions) => {
+	if (options.first_name.length <= 2) {
+		return [
+			{
+				field: 'first name',
+				message: 'first name must be greater than 2',
+			},
+		];
+	}
+
+	if (options.last_name.length <= 2) {
+		return [
+			{
+				field: 'last name',
+				message: 'last name must be greater than 2',
+			},
+		];
+	}
+
+	if (options.email.length <= 5 || !options.email.includes('@')) {
+		return [
+			{
+				field: 'email',
+				message: 'invalid email',
+			},
+		];
+	}
+
+	if (options.password.length <= 5) {
+		return [
+			{
+				field: 'password',
+				message: 'password must at least be 6',
+			},
+		];
+	}
+
+	return null;
+};
