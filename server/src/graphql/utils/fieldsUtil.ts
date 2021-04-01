643947bdf5971461a0d2dca6836f161156b2728e
alaminyusuf <alaminyusuf131@gmail>
2021-04-01 13:46:40 +0100

    added field util helper

---
+++ server/src/graphql/utils/fieldsUtil.ts
@@ -0,0 +1,19 @@
+import { ObjectType, Field } from 'type-graphql';
+import { User } from '../../models/User';
+@ObjectType()
+class ResponseError {
+	@Field(() => String)
+	field: string;
+
+	@Field(() => String)
+	message: string;
+}
+
+@ObjectType()
+export class UserResponse {
+	@Field(() => User, { nullable: true })
+	user?: User;
+
+	@Field(() => ResponseError, { nullable: true })
+	error?: ResponseError;
+}
