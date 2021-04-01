387f2c6fbfc74f4fb49f1baabab4964ae45a7e41
alaminyusuf <alaminyusuf131@gmail>
2021-04-01 23:39:55 +0100

    implemented single user query

--- server/src/graphql/Query/user.ts
+++ server/src/graphql/Query/user.ts
@@ -1,4 +1,4 @@
-import { Resolver, Query } from 'type-graphql';
+import { Resolver, Query, Arg } from 'type-graphql';
 import { User } from '../../models/User';
 
 @Resolver()
@@ -6,5 +6,10 @@ export class UserQuery {
 	@Query(() => [User])
 	async users(): Promise<User[] | undefined> {
 		return await User.find();
+	}
+
+	@Query(() => User)
+	async user(@Arg('id', () => String) id: string): Promise<User | undefined> {
+		return await User.findOne(id);
 	}
 }
